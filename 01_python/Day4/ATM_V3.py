a6ff1393294f60862b761f72464d5fcdcd14911a
Nitin <nitin2000mittal.com>
2026-07-18 19:55:41 +0530

    make improvements to ATM_V3

--- 01_python/Day4/ATM_V3.py
+++ 01_python/Day4/ATM_V3.py
@@ -1,34 +1,43 @@
-#Building a simple ATM application
+# Building a simple ATM application
 
 
 def showMenu():
-     
-         print('''Welcome to ATM
+
+    print("""Welcome to ATM
 
                 1. Check Balance
                 2. Deposit
                 3. Withdraw
-                4. Exit''')
-    
-         option = int(input("Please enter one of the options above:"))
-         return option
-     
-     
+                4. Exit""")
+
+    option = int(input("Please enter one of the options above:"))
+    return option
+
+
+def getAmount():
+    amount = int(input("Please enter the amount:"))
+    return amount
+
+def printAmount(amount):
+    print(f"Updated balance : {amount}")
 
 
 
-def deposit(balance,amount):
-    #global currentBalance 
-    
-    if(amount<0):
+
+def deposit(balance, amount):
+    # global currentBalance
+
+    if amount < 0:
         print("Invalid amount")
+        return balance
     else:
         balance = balance + amount
         return balance
 
-def withdraw(balance,amount):
-    #global currentBalance
-    if(amount>balance):
+
+def withdraw(balance, amount):
+    # global currentBalance
+    if amount > balance:
         print("Insufficient Balance")
         return balance
     else:
@@ -36,42 +45,32 @@ def withdraw(balance,amount):
         return balance
 
 
-
-
-
 def atmMain():
-    accounts = {
-    "nitin": 10000,
-    "rahul": 5000,
-    "priya": 25000
-}
+    accounts = {"nitin": 10000, "rahul": 5000, "priya": 25000}
     name = input("Please enter your name:")
     if name in accounts:
-         running = True
-         balance = accounts[name]
-         while(running):
-
+        running = True
+        # balance = accounts[name]
+        while running:
             option = showMenu()
-            
 
-            if(option==1):
-               print(f"Balance : {balance}") 
-            elif(option==2):
-                amount = int(input("Please enter the amount:"))
-                balance = deposit(balance,amount)
-                print(f"Updated balance : {balance}") 
-            elif(option==3):
-                amount = int(input("Please enter the amount:"))
-                balance = withdraw(balance,amount)  
-                print(f"Updated balance : {balance}") 
-            elif(option==4):
+            if option == 1:
+                print(f"Balance : {accounts[name]}")
+            elif option == 2:
+                amount = getAmount()
+                accounts[name] = deposit(accounts[name], amount)
+                printAmount(accounts[name])
+            elif option == 3:
+                amount = getAmount()
+                accounts[name] = withdraw(accounts[name], amount)
+                printAmount(accounts[name])
+            elif option == 4:
                 print("Thank you for using our ATM.")
                 running = False
-            else: 
+            else:
                 print("Invalid option")
     else:
         print("Account not found")
-    
 
 
 atmMain()
--- 01_python/Day4/exercise4.py
+++ 01_python/Day4/exercise4.py
@@ -1,13 +1,17 @@
-#remove even
+# remove even
+
 
 def removeEven():
-    numList = [2,5,8,11,14,17,20]
+    numList = [2, 5, 8, 11, 14, 17, 20]
+    newList = [num for num in numList if num % 2 != 0]
+    print(newList)
 
-    for num in numList:
+    """for num in numList:
         if num%2==0:
             numList.remove(num)
         else:
             pass
-    print(numList)
+    print(numList)"""
+
 
 removeEven()
